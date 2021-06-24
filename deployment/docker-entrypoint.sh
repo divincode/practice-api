be83c1fb39772de3f6d8ea80c9d2d6d07bad06e9
divincode <devadiga.1@iitj.ac.in>
2021-06-24 10:45:39 +0530

    Made some changes in the docker entry point sh

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -8,7 +8,9 @@ if ! whoami &> /dev/null; then
     # write a line in /etc/passwd for the Arbitrary User ID in the 'root' group
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
   fi
-fiif [ "$1" = 'supervisord' ]; then
+fi
+
+if [ "$1" = 'supervisord' ]; then
     exec /usr/bin/supervisord
 fi
 
